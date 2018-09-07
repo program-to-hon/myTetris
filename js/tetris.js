a5579ff6a2e712d1ee5b002d1f64ac30f8df4d27
program-to-hon <program.to.hon@gmail.com>
2018-09-07 13:48:02 +0900

    resposive

--- js/tetris.js
+++ js/tetris.js
@@ -353,7 +353,7 @@ $(window).resize(function() {
 
     function canvas_resize(){
         //var windowWidth=window.innerWidth;
-        var windowHeight=window.innnerHeight;
+        var windowHeight=window.innerHeight;
         //theCanvas.setAttribute('width',windowWidth);
         theCanvas.setAttribute('height',windowHeight);
     }
